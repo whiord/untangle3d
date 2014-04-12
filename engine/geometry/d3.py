ddbb7dcf3172b7ffb0dd25e1b6d3aef35f3aa666
Vladislav Eliseev <vl.eliseev@rambler.ru>
2014-04-13 02:58:07 +0400

    Added Point3D functionality

--- engine/geometry/d3.py
+++ engine/geometry/d3.py
@@ -1,8 +1,57 @@
 __author__ = 'whiord'
 
+from math import *
+
+COORD_SYSTEM_CARTESIAN = 0
+COORD_SYSTEM_SPHERICAL = 1
+
 
 class Point3D:
-    def __init__(self, alpha, beta, rho):
-        self.alpha = alpha
-        self.beta = beta
-        self.rho = rho
+    def __init__(self, p1, p2, p3, cs=COORD_SYSTEM_CARTESIAN):
+        self.cs = cs
+        if cs == COORD_SYSTEM_SPHERICAL:
+            self.alpha = p1
+            self.beta = p2
+            self.rho = p3
+        elif cs == COORD_SYSTEM_CARTESIAN:
+            self.x = p1
+            self.y = p2
+            self.z = p3
+        else:
+            raise ValueError("Unknown coordinate system")
+
+    def __sub__(self, other):
+        a = self.to_cartesian()
+        b = other.to_cartesian()
+        res = Point3D(a.x - b.x, a.y - b.y, a.z - b.z, COORD_SYSTEM_CARTESIAN)
+        return res if self.cs == COORD_SYSTEM_CARTESIAN else res.to_spherical()
+
+    def __add__(self, other):
+        a = self.to_cartesian()
+        b = other.to_cartesian()
+        res = Point3D(a.x + b.x, a.y + b.y, a.z + b.z, COORD_SYSTEM_CARTESIAN)
+        return res if self.cs == COORD_SYSTEM_CARTESIAN else res.to_spherical()
+
+    def __mul__(self, other):
+        a = self.to_cartesian()
+        b = other.to_cartesian()
+        return a.x * b.x + a.y * b.y + a.z * b.z
+
+    def to_cartesian(self):
+        if self.cs == COORD_SYSTEM_SPHERICAL:
+            x = self.rho * cos(self.alpha) * cos(self.beta)
+            y = self.rho * sin(self.alpha) * cos(self.beta)
+            z = self.rho * sin(self.beta)
+            return Point3D(x, y, z, COORD_SYSTEM_CARTESIAN)
+        else:
+            return Point3D(self.x, self.y, self.z, COORD_SYSTEM_CARTESIAN)
+
+    def to_spherical(self):
+        if self.cs == COORD_SYSTEM_CARTESIAN:
+            rho = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+            rho_xy = (self.x ** 2 + self.y ** 2) ** 0.5
+            alpha = asin(self.y / rho_xy)
+            beta = asin(self.z / rho)
+            return Point3D(alpha, beta, rho, COORD_SYSTEM_SPHERICAL)
+        else:
+            return Point3D(self.alpha, self.beta, self.rho, COORD_SYSTEM_SPHERICAL)
